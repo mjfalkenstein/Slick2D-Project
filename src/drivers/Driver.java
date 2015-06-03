36f89f35be4a0604ee64e9c6d336a3d6b7fb8fb8
Michael Falkenstein <michaeljfalkenstein@gmail.com>
2015-06-03 13:20:24 -0400

    Pushing a preliminary main menu, including four buttons and a simple background animation.
    The 'quit' button is the only button that does anything

--- src/drivers/Driver.java
+++ src/drivers/Driver.java
@@ -5,31 +5,63 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
+/**
+ * The main driver class, initializes all the game states and handles the initial display mode
+ */
 public class Driver extends StateBasedGame{
 	
 	StateBasedGame game;
+	GameContainer gc;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param name - the name of the game object
+	 */
 	public Driver(String name) {
 		super(name);
 	}
 	
-	public void init(GameContainer container, StateBasedGame game) throws SlickException{
+	/**
+	 * Called at program startup
+	 * 
+	 * @param gc - the game container
+	 * @param game - the game itself
+	 * 
+	 * @throws SlickException
+	 */
+	public void init(GameContainer gc, StateBasedGame game) throws SlickException{
+		this.gc = gc;
 		this.game = game;
 	}
 
-	public void initStatesList(GameContainer arg0) throws SlickException {
-		addState(new TestLevel());
+	/**
+	 * Called at program startup
+	 * 
+	 * Used to initialize all of the other screens in the game
+	 */
+	public void initStatesList(GameContainer gc) throws SlickException {
+		addState(new MainMenu());
 		
-		this.enterState(0);
+		enterState(new MainMenu().getID());
 	}
 	
+	/**
+	 * Called at program startup 
+	 * 
+	 * Used to initialize everything in the window, including title, size, etc.
+	 * Will eventually be used for loading saved games, user settings, etc.
+	 * 
+	 * @param args - command line arguments
+	 */
 	public static void main(String[] args){
 		AppGameContainer container;
 		try{
 			container = new AppGameContainer(new Driver("TEST"));
-			container.setDisplayMode(640, 480, false);
+			container.setDisplayMode(container.getScreenWidth(), container.getScreenHeight(), true);
 			container.setShowFPS(false);
 			container.setTargetFrameRate(60);
+			container.setAlwaysRender(true);
 			container.start();
 		}catch(Exception e){
 			e.printStackTrace();
